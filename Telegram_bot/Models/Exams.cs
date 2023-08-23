cf483ca3928e2b5c10f6e192f5ed8b20a1c1387a
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2023-08-23 22:47:59 +0500

    1

--- Telegram_bot/Models/Exams.cs
+++ Telegram_bot/Models/Exams.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,14 @@ namespace Telegram_bot.Models
       
         static string token = "YOUR_BOT_TOKEN_HERE";
 
-     
 
+        private List<int> exam = new List<int>()
+        {
+          
+
+        };
+
+        public List<int> Exam { get => exam; set => exam = value; }
 
         static async Task HandleCommandAsync(Telegram.Bot.Types.Message message)
         {
@@ -22,7 +29,7 @@ namespace Telegram_bot.Models
                 throw new ArgumentNullException(nameof(message));
             }
 
-
+            
 
 
 
